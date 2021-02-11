20265a2ef79c6fd5003cc1b134e1a404a02a9bd0
张亚飞 <672233580@qq.com>
2021-02-11 18:01:06 +0800

    飞

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -7,12 +7,50 @@
 
 import SwiftUI
 
+
+var health : Bool = true
+
+var money : Double = 10000
+
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, new year!")
             .padding()
+        Button(action: {
+            newYear()
+        }, label: {
+            Text("祝福")
+        })
     }
+    
+   
+    
+    func newYear() {
+        while(true) {
+            
+            health = true
+            
+            money += obtainMoreMoney()
+            
+            if(meetSomething()) {
+                print("NICE！")
+            }
+        }
+    }
+    
+    func obtainMoreMoney() -> Double {
+        
+        1000000000000000000000.0
+    }
+    
+    func meetSomething() -> Bool{
+        true
+    }
+    
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
